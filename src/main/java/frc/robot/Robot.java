067100a68aabd06e28727fa129a2dce5cd66ac30
Ryan Yohnk <ryanyohnk@myanmar-2.local>
2021-10-06 12:12:42 -0500

    Initial project files and Phoenix vendordeps

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,100 @@
+// Copyright (c) FIRST and other WPILib contributors.
+
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the methods corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class
+ * or the package after creating this project, you must also update the build.gradle file in the
+ * project.<br>
+ * <br>
+ * This class, via its super class, provides the following methods which are called by the main loop:<br>
+ * <br>
+ *   - startCompetition(), at the appropriate times:<br>
+ * <br>
+ *   - robotInit() -- provide for initialization at robot power-on<br>
+ * <br>
+ * init methods -- each of the following methods is called once when the appropriate mode is entered:<br>
+ *     - disabledInit()   -- called each and every time disabled is entered from another mode<br>
+ *     - autonomousInit() -- called each and every time autonomous is entered from another mode<br>
+ *     - teleopInit()     -- called each and every time teleop is entered from another mode<br>
+ *     - testInit()       -- called each and every time test is entered from another mode<br>
+ * <br>
+ * periodic methods -- each of these methods is called on an interval:<br>
+ *   - robotPeriodic()<br>
+ *   - disabledPeriodic()<br>
+ *   - autonomousPeriodic()<br>
+ *   - teleopPeriodic()<br>
+ *   - testPeriodic()<br>
+ */
+public class Robot extends TimedRobot
+{
+    /**
+     * This method is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit()
+    {
+
+    }
+
+    @Override
+    public void robotPeriodic()
+    {
+
+    }
+
+    @Override
+    public void autonomousInit()
+    {
+
+    }
+
+    @Override
+    public void autonomousPeriodic()
+    {
+        
+    }
+
+    @Override
+    public void teleopInit()
+    {
+        
+    }
+
+    @Override
+    public void teleopPeriodic()
+    {
+        
+    }
+
+    @Override
+    public void disabledInit()
+    {
+
+    }
+
+    @Override
+    public void disabledPeriodic()
+    {
+
+    }
+
+    @Override
+    public void testInit()
+    {
+        
+    }
+
+    @Override
+    public void testPeriodic()
+    {
+        
+    }
+}
