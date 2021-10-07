cc528b9e48dbf6024b68abd7d2e3faab6c76da36
Ryan Yohnk <ryanyohnk@myanmar-2.local>
2021-10-07 14:27:07 -0500

    Added TODOs for homework

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,50 +5,35 @@
 
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
 
-/**
- * The VM is configured to automatically run this class, and to call the methods corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class
- * or the package after creating this project, you must also update the build.gradle file in the
- * project.<br>
- * <br>
- * This class, via its super class, provides the following methods which are called by the main loop:<br>
- * <br>
- *   - startCompetition(), at the appropriate times:<br>
- * <br>
- *   - robotInit() -- provide for initialization at robot power-on<br>
- * <br>
- * init methods -- each of the following methods is called once when the appropriate mode is entered:<br>
- *     - disabledInit()   -- called each and every time disabled is entered from another mode<br>
- *     - autonomousInit() -- called each and every time autonomous is entered from another mode<br>
- *     - teleopInit()     -- called each and every time teleop is entered from another mode<br>
- *     - testInit()       -- called each and every time test is entered from another mode<br>
- * <br>
- * periodic methods -- each of these methods is called on an interval:<br>
- *   - robotPeriodic()<br>
- *   - disabledPeriodic()<br>
- *   - autonomousPeriodic()<br>
- *   - teleopPeriodic()<br>
- *   - testPeriodic()<br>
- */
 public class Robot extends TimedRobot
 {
 
     private WPI_TalonSRX talon;
 
     private final int TALON_PID = 2;
+
     private final double GEAR_RATIO = 5;
     private final double PULSE_PER_ROT = 4506;
+    private int count = 0;
+
     private final int PID_IDX = 0;
     private final int TIMEOUT_MS = 1000;
-    private final double ZERO_POS = 14500.0;
 
-    private int TEST_ANGLE = 0;
-    private int count = 0;
+    /**
+     * TODO #1
+     * We need to find the encode value when the pieces of red tape are aligned.
+     * You can find this by slowly rotating the motor, printing out the position value, and noting when they're aligned.
+     */
+
+    private final double ZERO_POS = 0;
+
+
+    private double TEST_ANGLE = .5;
+
 
 
     /**
@@ -62,44 +47,58 @@ public class Robot extends TimedRobot
         talon.configFactoryDefault(TIMEOUT_MS);
         talon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, PID_IDX, TIMEOUT_MS);
         talon.setSensorPhase(true);
-        talon.configAllowableClosedloopError(0, PID_IDX, TIMEOUT_MS);
-
-        talon.config_kF(PID_IDX, 0.0, TIMEOUT_MS);
-        talon.config_kP(PID_IDX, 0.75, TIMEOUT_MS);
-        talon.config_kI(PID_IDX, 0.0, TIMEOUT_MS);
-        talon.config_kD(PID_IDX, 0.5, TIMEOUT_MS);
-
-        //Going to 100% makes the power supply unhappy
-        talon.configNominalOutputForward(0, TIMEOUT_MS);
-        talon.configNominalOutputReverse(0, TIMEOUT_MS);
-        talon.configPeakOutputForward(0.5, TIMEOUT_MS);
-        talon.configPeakOutputReverse(-0.5, TIMEOUT_MS);
     }
 
     @Override
     public void teleopInit()
     {
-        System.out.println("Start Pos: " + talon.getSelectedSensorPosition(0));
+
     }
 
     @Override
     public void teleopPeriodic()
     {
+        double currentPosition = talon.getSelectedSensorPosition(0);
+
         if(count++ % 25 == 0) {
-            System.out.println("Angle: " + TEST_ANGLE);
-            System.out.println("Current Pos: " + talon.getSelectedSensorPosition(0));
-            System.out.println("Desired Pos: " + percentToPulse(TEST_ANGLE));
-            System.out.println("Current Error: " + talon.getClosedLoopError());
+            System.out.println("Current Pos: " + currentPosition);
             System.out.println();
         }
 
-        talon.set(ControlMode.Position, percentToPulse(TEST_ANGLE));
+        /**
+         * TODO #2
+         * Once you have found the ZERO_POS value, you can start to use the percentToPulse/pulsesToPercent methods. Read the notes below to understand how that works.
+         * Make sure your TEST_ANGLE is set to .5
+         * Similar to last week, you'll need to write some code that moves the wheel to approach the correct angle.
+         * You can either use pulsesToPercent or percentToPulse to find your error.
+         */
+
+        // You can either get the desired position and compare that to current position
+        double desiredPosition = percentToPulse(TEST_ANGLE);
+
+        // Or you find the current angle and compare that to our TEST_ANGLE
+        double currentAngle = pulsesToPercent(currentPosition);
+
+
+        /**
+         * TODO #3
+         * There are some naive approach that work, but aren't great. Either they lead to error (angles not being correct)
+         * or your wheel oscillates back and forth. What are some ways we can fix this?
+         */
+
+        /**
+         * TODO #4
+         * What if we want to rotate different amounts during the competition? Is there a way to make this more modular?
+         * Dig into FRC's command based programming to get some ideas.
+         * https://docs.wpilib.org/en/stable/docs/software/commandbased/index.html
+         */
+
     }
 
     @Override
     public void disabledInit()
     {
-        System.out.println("Final Pos: " + talon.getSelectedSensorPosition(0));
+
     }
 
     // "Percent" refers to the percent of the total rotation
